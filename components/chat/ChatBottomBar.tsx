26ef958571dc94ec103b9417f27fffbcacad3fe0
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-08-19 18:32:58 +0530

    all chat sub comp ui done

---
+++ components/chat/ChatBottomBar.tsx
@@ -0,0 +1,99 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { Image as ImageIcon, Loader, ThumbsUp } from "lucide-react";
+import React from "react";
+import { Textarea } from "../ui/textarea";
+import { useState } from "react";
+import EmojiPicker from "./EmojiPicker";
+import { useRef } from "react";
+import { Button } from "../ui/button";
+import { SendHorizonal } from "lucide-react";
+import useSound from "use-sound";
+import { usePreferences } from "@/store/usePreferences";
+function ChatBottomBar() {
+  const [message, setmessage] = useState("");
+  const textArearef = useRef<HTMLTextAreaElement>(null);
+  const isPending = false;
+  const {soundEnabled} = usePreferences();
+
+  const [playSound1] = useSound("/sounds/keystroke1.mp3");
+	const [playSound2] = useSound("/sounds/keystroke2.mp3");
+	const [playSound3] = useSound("/sounds/keystroke3.mp3");
+	const [playSound4] = useSound("/sounds/keystroke4.mp3");
+
+  const playSoundFunctions = [playSound1, playSound2, playSound3, playSound4];
+
+	const playRandomKeyStrokeSound = () => {
+		const randomIndex = Math.floor(Math.random() * playSoundFunctions.length);
+		soundEnabled && playSoundFunctions[randomIndex]();
+	};
+
+
+
+  return (
+    <div className="p-2 flex justify-between w-full items-center gap-2">
+      {!message.trim() && (
+        <ImageIcon size={20} className="cursor-pointer text-muted-foreground" />
+      )}
+      <AnimatePresence>
+        <motion.div
+          layout
+          initial={{ opacity: 0, scale: 1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 1 }}
+          transition={{
+            opacity: { duration: 0.5 },
+            layout: {
+              type: "spring",
+              bounce: 0.15,
+            },
+          }}
+          className="w-full relative "
+        >
+          <Textarea
+            autoComplete="off"
+            placeholder="Enter the message"
+            rows={1}
+            className="w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background min-h-0"
+            value={message}
+            onChange={(e) => {
+              setmessage(e.target.value);
+              playRandomKeyStrokeSound();
+            }}
+          />
+          <div className="absolute right-2 bottom-0.5 ">
+            <EmojiPicker
+              onChange={(emoji) => {
+                setmessage(message + emoji);
+                if (textArearef.current) {
+                  textArearef.current.focus();
+                }
+              }}
+            />
+          </div>
+        </motion.div>
+        {message.trim() ? (
+          <Button
+            className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+            variant={"ghost"}
+            size={"icon"}
+            
+          >
+            <SendHorizonal size={20} className="text-muted-foreground" />
+          </Button>
+        ) : (
+          <Button
+            className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+            variant={"ghost"}
+            size={"icon"}
+          >
+            {!isPending && <ThumbsUp size={20} className="text-muted-foreground"/> }
+            {isPending && <Loader size={20} className="animate-spin"/> }
+          
+          </Button>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default ChatBottomBar;
--- components/chat/ChatLayout.tsx
+++ components/chat/ChatLayout.tsx
@@ -1,58 +1,86 @@
-"use client"
-import React, { useEffect } from 'react'
-import { ResizablePanelGroup,ResizablePanel,ResizableHandle } from '../ui/resizable'
-import { useState } from 'react'
-import { cn } from '@/lib/utils'
-
+"use client";
+import React, { useEffect } from "react";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "../ui/resizable";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Sidebar from "../Sidebar";
+import MessageContainer from "./MessageContainer";
 
 interface ChatLayoutprops {
-    defaultLayout : number[] | undefined
+  defaultLayout: number[] | undefined;
 }
 
-// when first time user visits the chat page the default dimensions are 320px and 480px 
+// when first time user visits the chat page the default dimensions are 320px and 480px
 
-function ChatLayout({defaultLayout=[320,480]}:ChatLayoutprops) {
-    const [isMobile,setisMobile] = useState(false);
-    const [isCollapsed,setisCollapsed] = useState(false);
+function ChatLayout({ defaultLayout = [320, 480] }: ChatLayoutprops) {
+  const [isMobile, setisMobile] = useState(false);
+  const [isCollapsed, setisCollapsed] = useState(false);
 
-    useEffect(() => {
-        const checkScreenwidth = ()=>{
-            setisMobile(window.innerWidth <=760)
-        }
+  useEffect(() => {
+    const checkScreenwidth = () => {
+      setisMobile(window.innerWidth <= 760);
+    };
 
-        checkScreenwidth();
+    checkScreenwidth();
 
-        window.addEventListener("resize",checkScreenwidth)
-        return () => {
-            window.removeEventListener("resize",checkScreenwidth)
-        };
-    }, []);
+    window.addEventListener("resize", checkScreenwidth);
+    return () => {
+      window.removeEventListener("resize", checkScreenwidth);
+    };
+  }, []);
 
   return (
-    <ResizablePanelGroup direction='horizontal'
-    className='h-full items-stretch bg-background rounded-lg'
-    onLayout={(sizes:number[])=>{
-        document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`
-    }}>
-        <ResizablePanel defaultSize={defaultLayout[0]}
-            collapsedSize={8}
-            collapsible={true}
-            minSize={isMobile ? 0:24}
-            maxSize={isMobile ? 8:30}
-            onCollapse={()=>{
-                setisCollapsed(true);
-                document.cookie = `react-resizable-panels:collapsed=true`;
-            }}
-            onExpand={()=>{
-                setisCollapsed(false);
-                document.cookie = `react-resizable-panels:collapsed=false`;
-            }}
-            className={cn(isCollapsed && "min-w-[80px] transition-all duration-300 ease-in-out")}>Sidebar</ResizablePanel>
-        <ResizableHandle withHandle/>
-        <ResizablePanel>Right</ResizablePanel>
-
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="h-full items-stretch bg-background rounded-lg"
+      onLayout={(sizes: number[]) => {
+        document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+          sizes
+        )}`;
+      }}
+    >
+      <ResizablePanel
+        defaultSize={defaultLayout[0]}
+        collapsedSize={8}
+        collapsible={true}
+        minSize={isMobile ? 0 : 24}
+        maxSize={isMobile ? 8 : 30}
+        onCollapse={() => {
+          setisCollapsed(true);
+          document.cookie = `react-resizable-panels:collapsed=true`;
+        }}
+        onExpand={() => {
+          setisCollapsed(false);
+          document.cookie = `react-resizable-panels:collapsed=false`;
+        }}
+        className={cn(
+          isCollapsed && "min-w-[80px] transition-all duration-300 ease-in-out"
+        )}
+      >
+        <Sidebar isCollapsed={isCollapsed} />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+        { /* <div className="flex justify-center items-center h-full w-full px-10">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <img
+                src="/logo.png"
+                alt="Logo"
+                className="w-full md:w-2/3 lg:w-1/2"
+              />
+              <p className="text-muted-foreground text-center">
+                Click on a chat to view the messages
+              </p>
+            </div>
+          </div>*/}
+          <MessageContainer/>
+      </ResizablePanel>
     </ResizablePanelGroup>
-  )
+  );
 }
 
-export default ChatLayout
+export default ChatLayout;
