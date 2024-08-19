43ab5df34b4f278d6a93dedfef03eb4dfa372e58
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-08-19 13:27:52 +0530

    animation addition done

--- components/chat/ChatLayout.tsx
+++ components/chat/ChatLayout.tsx
@@ -1,16 +1,20 @@
 "use client"
-
 import React, { useEffect } from 'react'
 import { ResizablePanelGroup,ResizablePanel,ResizableHandle } from '../ui/resizable'
 import { useState } from 'react'
+import { cn } from '@/lib/utils'
+
+
 interface ChatLayoutprops {
     defaultLayout : number[] | undefined
 }
 
 // when first time user visits the chat page the default dimensions are 320px and 480px 
+
 function ChatLayout({defaultLayout=[320,480]}:ChatLayoutprops) {
     const [isMobile,setisMobile] = useState(false);
-    const [collapsed,setisCollapsed] = useState(false)
+    const [isCollapsed,setisCollapsed] = useState(false);
+
     useEffect(() => {
         const checkScreenwidth = ()=>{
             setisMobile(window.innerWidth <=760)
@@ -42,7 +46,8 @@ function ChatLayout({defaultLayout=[320,480]}:ChatLayoutprops) {
             onExpand={()=>{
                 setisCollapsed(false);
                 document.cookie = `react-resizable-panels:collapsed=false`;
-            }}>Sidebar</ResizablePanel>
+            }}
+            className={cn(isCollapsed && "min-w-[80px] transition-all duration-300 ease-in-out")}>Sidebar</ResizablePanel>
         <ResizableHandle withHandle/>
         <ResizablePanel>Right</ResizablePanel>
 
